e08cf79add56dc52d909fb6897f7400eaae82063
Tamas Kalman <ktamas77@gmail.com>
2015-03-05 06:26:47 -0800

    dynamic driver loading through linxup api

--- app.js
+++ app.js
@@ -1,48 +1,18 @@
 (function(){
-	var app = angular.module('linxup-streetview', [ ]);
+	var app = angular.module('linxup-streetview', []);
 
-	app.controller('LinxupController', function(){
-		this.vehicles = demoPositions;
-	});
+	app.controller('LinxupController', ['$http', function($http) {
+		var linxup = this;
+    linxup.drivers = {};
 
-	var demoPositions = {
-                   "data": {
-                       "positions": [ {
-                           "date": 1381254908000,
-                           "label": "Vehicle One",
-                           "latitude": 38.6558,
-                           "longitude": -90.5618,
-                           "heading": "S",
-                           "direction": 163,
-                           "speed": 80,
-                           "speeding": true,
-                           "behaviorCd": "HAC",
-                           "estSpeedLimit": 70
-                       }, {
-                           "date": 1383775826000,
-                           "label": "Vehicle Two",
-                           "latitude": 38.598,
-                           "longitude": -90.4347,
-                           "heading": "N",
-                           "direction": 0,
-                           "speed": 0,
-                           "speeding": null,
-                           "behaviorCd": null,
-                           "estSpeedLimit": null
-                       }, {
-                           "date": 1383743827000,
-                           "label": "Vehicle Three",
-                           "latitude": 38.6553,
-                           "longitude": -90.5619,
-                           "heading": "SE",
-                           "direction": 146,
-                           "speed": 45,
-                           "speeding": null,
-                           "behaviorCd": null,
-                           "estSpeedLimit": null
-					}] },
-                   "responseType": "Success"
-               }
-
-
+    $http.post(
+        'https://www.linxup.com/ibis/rest/linxupmobile/login',
+        {
+          username: '###',
+          password: '###'
+        }
+    ).success(function(response) {
+        linxup.drivers = response.data.drivers;
+    });
+	}]);
 })();
