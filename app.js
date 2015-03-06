385e4bef8039ee7ac083ef779beaa1d3e7665499
Tamas Kalman <ktamas77@gmail.com>
2015-03-05 16:35:08 -0800

    tabs fixed

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 (function(){
-	var app = angular.module('linxup-streetview', []);
+  var app = angular.module('linxup-streetview', []);
 
-	app.controller('LinxupController', ['$http', function($http) {
-		var linxup = this;
+  app.controller('LinxupController', ['$http', function($http) {
+    var linxup = this;
     linxup.drivers = {};
 
     $http.post(
-        'https://www.linxup.com/ibis/rest/linxupmobile/login',
-        {
-          username: configData.username,
-          password: configData.password
-        }
+      'https://www.linxup.com/ibis/rest/linxupmobile/login',
+      {
+        username: configData.username,
+        password: configData.password
+      }
     ).success(function(response) {
-        linxup.drivers = response.data.drivers;
+      linxup.drivers = response.data.drivers;
     });
-	}]);
+  }]);
 })();
